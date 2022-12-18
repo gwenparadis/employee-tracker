aa99d274d1e84c08296a7b4ed2a4a41c1fbdc5da
gwenparadis <gwendelynparadis2@gmail.com>
2022-12-18 17:36:06 -0500

    initial commit, file setup, and beginning sql

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS employeetracker_db;
+CREATE DATABASE employeetracker_db;
+
+USE employeetracker_db;
+
+CREATE TABLE department;
+
+CREATE TABLE role;
+
+CREATE TABLE employee;
---
+++ index.js
@@ -0,0 +1,4 @@
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+const mysql2 = require('mysql2');
+
