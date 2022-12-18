7ceb14d9a157c2f17e69850e5035b04d7a874ed0
gwenparadis <gwendelynparadis2@gmail.com>
2022-12-18 17:56:59 -0500

    set up database based on assets png file

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,37 @@
+-- create the database for the project
 DROP DATABASE IF EXISTS employeetracker_db;
 CREATE DATABASE employeetracker_db;
 
 USE employeetracker_db;
 
-CREATE TABLE department;
+--create the department table based on the asset/png file
+CREATE TABLE department (
+    id INT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
 
-CREATE TABLE role;
+--create the role table based from on asset/png file
+CREATE TABLE role (
+    id INT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (deparmtent_id),
+    REFERENCES department(id)
+);
 
-CREATE TABLE employee;
+--create the employee table based on the asset/png file
+CREATE TABLE employee (
+    id INT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id),
+    REFERENCES role(id),
+    PRIMARY KEY (id)
+    --FOREIGN KEY (manager_id),???
+    --REFERENCES employee(id)???
+);
