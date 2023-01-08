5576b10eb8f88869515cdb2c2b2c2a4f2a321e5a
gwenparadis <gwendelynparadis2@gmail.com>
2023-01-08 14:06:23 -0500

    begin correcting the mysql2 queries without express

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-const express = require('express');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
 
 // Connect to database
-const db = mysql.createConnection(
+const db = await mysql.createConnection(
   {
     host: 'localhost',
     // MySQL username,
@@ -24,127 +18,96 @@ const db = mysql.createConnection(
 );
 
 inquirer
-    .prompt([
-        {
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a role', 'Add an employee', 'Update an Employee Role'],
-        }
-    ])
-    .then(console.log('You did it!'));
-
-
-// edit the code below as reference is taken from mini project
-
+  .prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a role', 'Add an employee', 'Update an Employee Role'],
+    }
+  ])
+  .then(console.log('You did it!'));
 
 // view departments
-app.get('/api/departments', (req, res) => {
-    const sql = `SELECT id, movie_name AS title FROM movies`;
-    
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-         return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
+const viewDepartments = function () {
+  db.query(`SELECT id, movie_name AS title FROM movies`, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    return rows;
   });
+}
 
 // view roles
-app.get('/api/roles', (req, res) => {
-    const sql = `SELECT id, movie_name AS title FROM movies`;
-    
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-         return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
+const viewRoles = function () {
+  db.query(`SELECT id, movie_name AS title FROM movies`, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    return rows;
   });
+}
 
 // view employees
-app.get('/api/employees', (req, res) => {
-    const sql = `SELECT id, movie_name AS title FROM movies`;
-    
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-         return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
+const viewEmployees = function () {
+  db.query(`SELECT id, movie_name AS title FROM movies`, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    return rows;
   });
+}
 
 // add department
-app.post('/api/new-department', ({ body }, res) => {
-    const sql = `INSERT INTO departments (name)
-      VALUES (?)`;
-    const params = [body.name];
-    
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
-    });
+const addDepartment = function () {
+  const params = [body.name];
+
+  db.query(`INSERT INTO departments (name)
+      VALUES (?)`, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    return body;
   });
+}
 
 // add role
-app.post('/api/new-role', ({ body }, res) => {
-    const sql = `INSERT INTO roles (title, salary, department_id)
-      VALUES (?)`;
-    const params = [body.title, body.salary, body.department_id];
-    
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
-    });
+const addRole = function () {
+  const params = [body.title, body.salary, body.department_id];
+
+  db.query(`INSERT INTO roles (title, salary, department_id)
+  VALUES (?)`, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    return body;
   });
+}
 
 // add employee
-app.post('/api/new-employee', ({ body }, res) => {
-    const sql = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
-      VALUES (?)`;
-    const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
-    
-    db.query(sql, params, (err, result) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: body
-      });
-    });
-  });
-  
+const addEmployee = function () {
+  const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+
+  db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id)
+  VALUES (?)`, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    return body;
+  }):
+}
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
-    res.status(404).end();
-  });
+  res.status(404).end();
+});
 
 // Listening
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Server running on port ${PORT}`);
+});
